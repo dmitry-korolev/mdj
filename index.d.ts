abc50e6488ba9df0928dccf07b2e066818f3a53e
Dmitry Korolev <dk@mustdigital.ru>
2017-05-16 23:27:02 +0300

    Add exports

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,25 @@
-import { NodeItem, Tokenizer, Parsed } from './src/models'
+import {
+  NodeSpace,
+  NodeText,
+  NodeStrong,
+  NodeEm,
+  NodeStrikethrough,
+  NodeCode,
+  NodeLineBreak,
+  NodeLink,
+  NodeImage,
+  NodeHeading,
+  NodeBlockquote,
+  NodeParagraph,
+  NodeCodeBlock,
+  NodeHR,
+  NodeTable,
+  NodeListItem,
+  NodeList,
+  NodeItem,
+  Tokenizer,
+  Parsed
+} from './src/models'
 
 type InlineParser = (source: string, inlineLexer?: Tokenizer) => Parsed<NodeItem>
 type BlockParser = (source: string, blockLexer?: Tokenizer, inlineLexer?: Tokenizer) => Parsed<NodeItem>
@@ -13,4 +34,27 @@ declare const mdj: () => MDJ
 declare const parse: (source: string) => NodeItem[]
 
 export default mdj
-export { parse }
+export {
+  parse,
+  NodeSpace,
+  NodeText,
+  NodeStrong,
+  NodeEm,
+  NodeStrikethrough,
+  NodeCode,
+  NodeLineBreak,
+  NodeLink,
+  NodeImage,
+  NodeHeading,
+  NodeBlockquote,
+  NodeParagraph,
+  NodeCodeBlock,
+  NodeHR,
+  NodeTable,
+  NodeListItem,
+  NodeList,
+  NodeItem,
+  Tokenizer,
+  Parsed,
+  MDJ
+}
